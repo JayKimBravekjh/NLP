26155695f1a2a2586a95ccdf4366c001f72b2b4a
Jay Kim  (Data Scientist) <jay.kim07@yahoo.com>
2016-03-16 11:40:32 -0700

    countvectorizer of papers
    countvectorizer of papers

---
+++ countvectorizer.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+# You can read a CSV file like this
+papers = pd.read_csv("Papers.csv")
+
+cv = CountVectorizer(ngram_range=(2, 2), max_features = 500, stop_words='english')
+cv.fit(papers.PaperText)
+
+X = cv.transform(papers.PaperText)
+counts = X.sum(axis=0)
+
+df = pd.DataFrame({'Bigrams': cv.get_feature_names(), 'Count': counts.tolist()[0]})
+df.sort_values(by='Count', ascending=False, inplace=True)
+
+print(df.head(10))
